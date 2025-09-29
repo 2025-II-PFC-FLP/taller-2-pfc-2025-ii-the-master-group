b1156175c92c8b002ebbb29ce738c2b37de65e84
Dev6007 <gabriel.bernal@correounivalle.edu.co>
2025-09-29 14:42:29 -0500

    ConjuntosDifusos (Incompleto)
    Interpretación de "(n/(n+d)^e)" completada, parametros de funcionamiento del inicializador pendientes. (con tail recursion)

---
+++ app/src/main/scala/taller/ConjuntosDifusos.scala
@@ -0,0 +1,13 @@
+package taller
+
+class ConjuntosDifusos {
+  def grande(d: Int, e: Int): ConjuntosDifusos = {
+    require(d >= 1 && e > 1)
+    val cantInicial = 1
+    val cantFinal = 10000
+
+    @annotation.tailrec
+    def inicializador(): ConjuntosDifusos = {}
+  }
+
+}
