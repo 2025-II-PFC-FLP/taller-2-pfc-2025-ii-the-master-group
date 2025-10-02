25a0e1e0e434770c270a9a8c7b3ccf5a78d199eb
Dev6007 <gabriel.bernal@correounivalle.edu.co>
2025-10-02 14:00:55 -0500

    ConjuntosDifusos (Completo)
    Se modifico el codigo para implementar Map y la biblioteca Math, se agrego un identificador que indica si "n" es grande o no. Se termino de implementar el inicializador. Las tests siguen pendientes.

--- app/src/main/scala/taller/ConjuntosDifusos.scala
+++ app/src/main/scala/taller/ConjuntosDifusos.scala
@@ -1,13 +1,27 @@
 package taller
 
 class ConjuntosDifusos {
-  def grande(d: Int, e: Int): ConjuntosDifusos = {
+  type ConjDifuso = Map[Int, Double] //se uso Map aqui para despues implementarlo
+                                    // como un acumulador
+
+  def identificadorN(n: Int, d: Int, e: Int): Double =
+    Math.pow(n.toDouble / (n + d).toDouble, e.toDouble) //se uso la biblioteca Math en este caso
+
+  def grande(d: Int, e: Int): ConjDifuso = {
     require(d >= 1 && e > 1)
+
     val cantInicial = 1
     val cantFinal = 10000
 
     @annotation.tailrec
-    def inicializador(): ConjuntosDifusos = {}
-  }
+    def inicializador(n: Int, acc: ConjDifuso, f: (Int, Int, Int) => Double): ConjDifuso = {
+      if (n > cantFinal) acc
+      else {
+        val valor = f(n, d, e)
+        inicializador(n + 1, acc + (n -> valor), f)
+      }
+    }
 
+    inicializador(cantInicial, Map.empty, identificadorN)
+  }
 }
